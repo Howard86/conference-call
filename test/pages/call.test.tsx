6e56e09bd48d0e5a7017fa6a21c561fe90e10bb6
Howard Tai <howardtai86@gmail.com>
2021-01-11 20:12:42 +0800

    fix: mock next/router

--- test/pages/call.test.tsx
+++ test/pages/call.test.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import CallPage from '@/pages/call';
 import { render } from '../test-utils';
 
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      push: (endpoint: string) => {
+        console.error(endpoint);
+      },
+    };
+  },
+}));
+
 describe('CallPage', () => {
   it('should render Call page', () => {
     render(<CallPage />, {});

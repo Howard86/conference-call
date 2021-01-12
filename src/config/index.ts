434345e468b48b7dd675fead1c4e5d63da2e4c15
Howard Tai <howardtai86@gmail.com>
2021-01-12 15:28:50 +0800

    feat: update config and add env check

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,24 @@
+const getEnvString = (field: string, defaultValue?: string): string => {
+  return process.env[field] || defaultValue;
+};
+
+const getEnvNumber = (field: string, defaultValue?: number): number => {
+  const result = getEnvString(field);
+
+  return typeof result === 'string' ? parseInt(result, 10) : defaultValue;
+};
+
 const config = {
   agora: {
-    appId: process.env.NEXT_PUBLIC_AGORA_APP_ID,
-    testToken: process.env.NEXT_PUBLIC_AGORA_TEST_TOKEN,
+    appId: getEnvString('NEXT_PUBLIC_AGORA_APP_ID'),
+    testToken: getEnvString('NEXT_PUBLIC_AGORA_TEST_TOKEN'),
   },
-  password: process.env.PASSWORD,
+  auth: {
+    hashSalt: getEnvString('HASH_SALT', 'salt'),
+    password: getEnvString('PASSWORD'),
+    jwtSecret: getEnvString('JWT_SECRET'),
+    jwtExpiration: getEnvNumber('JWT_EXPIRATION'),
+  },
 };
 
 export default config;
